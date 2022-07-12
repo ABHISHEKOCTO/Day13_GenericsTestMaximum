78c7d0343d2605ff67aaf7a747e71c10b7e41381
Abhishek <you@example.com>
2022-07-12 08:50:49 +0530

    Added:UC1 ~ max in 3 integers

---
+++ src/com/javaGenerics/FindTheMaximumNum.java
@@ -0,0 +1,35 @@
+package com.javaGenerics;
+import java.util.Scanner;
+public class FindTheMaximumNum {
+
+	
+		static Scanner sc = new Scanner(System.in);
+
+		/*
+		 * UC1 Given 3 Interger find the maximum
+		 */
+		public void maxmumIntegerNum(Integer A, Integer B, Integer C) {
+			Integer max = A;
+			if (B.compareTo(max) > 0) {
+				max = B;
+			}
+			if (C.compareTo(max) > 0) {
+				max = C;
+			}
+			System.out.println("The maximum Integer value is : " + max);
+		}
+
+		
+		public static void main(String[] args) {
+			FindTheMaximumNum g = new FindTheMaximumNum();
+			
+			g.maxmumIntegerNum(54, 56, 61);
+
+		}
+
+	}
+	
+	
+	
+	
+
